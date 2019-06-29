f6209687f72136e0f3e240f418e5500c39be996e
Chingiz Bayshurin <info@baydevgroup.net>
2019-06-29 20:11:21 +0300

    humanizeDiff improve

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -8,6 +8,7 @@ const val SECOND = 1000L
 const val MINUTE = 60 * SECOND
 const val HOUR = 60 * MINUTE
 const val DAY = 24 * HOUR
+const val YEAR = 365 * DAY
 
 
 fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
@@ -24,47 +25,77 @@ fun Date.add(value: Int, units: TimeUnit = TimeUnit.SECOND): Date {
         TimeUnit.MINUTE -> value * MINUTE
         TimeUnit.HOUR -> value * HOUR
         TimeUnit.DAY -> value * DAY
+        TimeUnit.YEAR -> value * YEAR
     }
     this.time = time
     return this
 }
 
-//TODO FIX ME!!!
+
 fun Date.humanizeDiff(): String {
 
-    val delta = Date().time - this.time
+    var delta = Date().time - this.time
+    var past = true
+    if (delta < 0) {
+        past = false
+        delta = -delta
+    }
 
+    fun plurals(i: Long, timeunit: TimeUnit): String {
+        return if(i in 0..1) {
+            when(timeunit) {
+                TimeUnit.SECOND ->  "$i секунду"
+                TimeUnit.MINUTE ->  "$i минуту"
+                TimeUnit.HOUR ->  "$i час"
+                TimeUnit.DAY ->  "$i день"
+                TimeUnit.YEAR ->  "$i год"
+            }
+        } else if(i in 2..4) {
+            when(timeunit) {
+                TimeUnit.SECOND ->  "$i секунды"
+                TimeUnit.MINUTE ->  "$i минуты"
+                TimeUnit.HOUR ->  "$i часа"
+                TimeUnit.DAY ->  "$i дня"
+                TimeUnit.YEAR ->  "$i года"
+            }
+        } else {
+            when(timeunit) {
+                TimeUnit.SECOND ->  "$i секунд"
+                TimeUnit.MINUTE ->  "$i минут"
+                TimeUnit.HOUR ->  "$i часов"
+                TimeUnit.DAY ->  "$i дней"
+                TimeUnit.YEAR ->  "$i лет"
+            }
+        }
+    }
 
-    return  if (delta < SECOND * 60) { "несколько секунд назад" }
+    return  if (delta < SECOND * 60) {
+                if(past) "несколько секунд назад"
+                else "через несколько секунд"
+    }
             else if (delta < MINUTE * 60) {
-
                 val del = delta/1000/60
-                when (del) {
-                    in 0..1 -> "${del} минуту назад"
-                    in 1..4 -> "${del} минуты назад"
-                    else -> "${del} минут назад"
-                }
+                if(past) plurals(del, TimeUnit.MINUTE) + " назад"
+                else "через " + plurals(del, TimeUnit.MINUTE)
             }
 
             else if (delta < HOUR  * 24)  {
                 val del = delta/1000/60/60
-                when (del) {
-                    in 0..1 -> "${del} час назад"
-                    in 1..4 -> "${del} часа назад"
-                    else -> "${del} часов назад"
-                }
-
+                if(past) plurals(del, TimeUnit.HOUR) + " назад"
+                else "через " + plurals(del, TimeUnit.HOUR)
             }
             else if (delta < DAY * 365)  {
                 val del = delta/1000/60/60/24
-                when (del) {
-                    in 0..1 -> "${del} день назад"
-                    in 1..4 -> "${del} дня назад"
-                    else -> "${del} дней назад"
-                }
-
+                if(past) plurals(del, TimeUnit.DAY) + " назад"
+                else "через " + plurals(del, TimeUnit.DAY)
             }
-            else "давно"
+            else {
+                val del = delta/1000/60/60/24/365
+                //                if(past) plurals(del, TimeUnit.YEAR) + " назад"
+                //                else "через " + plurals(del, TimeUnit.YEAR)
+                if(past) "более года назад"
+                else "через год"
+            }
 }
 
 
@@ -72,5 +103,6 @@ enum class TimeUnit {
     SECOND,
     MINUTE,
     HOUR,
-    DAY
+    DAY,
+    YEAR
 }
--- app/src/test/java/ru/skillbranch/devintensive/ExampleUnitTest.kt
+++ app/src/test/java/ru/skillbranch/devintensive/ExampleUnitTest.kt
@@ -184,23 +184,39 @@ class ExampleUnitTest {
 
     @Test
     fun test_of_humanizeDiff() {
-        assertEquals( "несколько секунд назад", Date().add(2, TimeUnit.MINUTE).humanizeDiff()  )
-
+        // ----- Past -----
+        assertEquals( "несколько секунд назад", Date().add(-2, TimeUnit.SECOND).humanizeDiff()  )
         assertEquals( "1 минуту назад", Date().add(-1, TimeUnit.MINUTE).humanizeDiff()  )
         assertEquals( "2 минуты назад", Date().add(-2, TimeUnit.MINUTE).humanizeDiff()  )
         assertEquals( "3 минуты назад", Date().add(-3, TimeUnit.MINUTE).humanizeDiff()  )
         assertEquals( "5 минут назад", Date().add(-5, TimeUnit.MINUTE).humanizeDiff()  )
-
         assertEquals( "1 час назад", Date().add(-1, TimeUnit.HOUR).humanizeDiff()  )
         assertEquals( "2 часа назад", Date().add(-2, TimeUnit.HOUR).humanizeDiff()  )
         assertEquals( "3 часа назад", Date().add(-3, TimeUnit.HOUR).humanizeDiff()  )
         assertEquals( "4 часа назад", Date().add(-4, TimeUnit.HOUR).humanizeDiff()  )
         assertEquals( "5 часов назад", Date().add(-5, TimeUnit.HOUR).humanizeDiff()  )
-
         assertEquals( "1 день назад", Date().add(-1, TimeUnit.DAY).humanizeDiff() )
         assertEquals( "4 дня назад", Date().add(-4, TimeUnit.DAY).humanizeDiff() )
         assertEquals( "5 дней назад", Date().add(-5, TimeUnit.DAY).humanizeDiff() )
         assertEquals( "100 дней назад", Date().add(-100, TimeUnit.DAY).humanizeDiff() )
+        assertEquals( "более года назад", Date().add(-400, TimeUnit.DAY).humanizeDiff()  )
+
+        // ----- Future ------
+        assertEquals( "через несколько секунд", Date().add(2, TimeUnit.SECOND).humanizeDiff()  )
+        assertEquals( "через 1 минуту", Date().add(1, TimeUnit.MINUTE).humanizeDiff()  )
+        assertEquals( "через 2 минуты", Date().add(2, TimeUnit.MINUTE).humanizeDiff()  )
+        assertEquals( "через 3 минуты", Date().add(3, TimeUnit.MINUTE).humanizeDiff()  )
+        assertEquals( "через 5 минут", Date().add(5, TimeUnit.MINUTE).humanizeDiff()  )
+        assertEquals( "через 1 час", Date().add(1, TimeUnit.HOUR).humanizeDiff()  )
+        assertEquals( "через 2 часа", Date().add(2, TimeUnit.HOUR).humanizeDiff()  )
+        assertEquals( "через 3 часа", Date().add(3, TimeUnit.HOUR).humanizeDiff()  )
+        assertEquals( "через 4 часа", Date().add(4, TimeUnit.HOUR).humanizeDiff()  )
+        assertEquals( "через 5 часов", Date().add(5, TimeUnit.HOUR).humanizeDiff()  )
+        assertEquals( "через 1 день", Date().add(1, TimeUnit.DAY).humanizeDiff() )
+        assertEquals( "через 4 дня", Date().add(4, TimeUnit.DAY).humanizeDiff() )
+        assertEquals( "через 5 дней", Date().add(5, TimeUnit.DAY).humanizeDiff() )
+        assertEquals( "через 148 дней", Date().add(148, TimeUnit.DAY).humanizeDiff() )
+        assertEquals( "через год", Date().add(400, TimeUnit.DAY).humanizeDiff()  )
     }
 
 
